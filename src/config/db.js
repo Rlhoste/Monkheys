e615ad4445a30b6028026f3156bf4fc7e64fc2e4
Rémi Lhoste <remlhoste@gmail.com>
2018-12-21 17:22:09 +0100

    Environnement Variable7

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,10 @@
+const mysql = require('mysql');
 module.exports = {
     database: 'process.env.MYSQL_ADDON_DB',
     username: 'process.env.MYSQL_ADDON_USER',
     password: 'process.env.MYSQL_ADDON_PASSWORD',
     options: {
-        host: '127.0.0.1',
+        host: 'process.env.MYSQL_ADDON_HOST',
         dialect: 'mysql'
     }
 }
